59bbd6fe2c1d9c8546ed36e36e928e8685dc7441
Sudarshan shinde <shindesudarshan070@gmail.com>
2021-05-01 23:12:44 +0530

    post route

---
+++ routes/cricket.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+const Cricket = require('../modal/cricket')
+
+
+router.post('/', async (req, res, next) => {
+    try {
+        let cricket = await Cricket.create(req.body)
+        res.json({ cricket })
+    } catch (error) {
+        next(error)
+    }
+})
+
+module.exports = router;
