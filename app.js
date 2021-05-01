d5536673bc4bf96d5e153d42ffccff19af1df561
Sudarshan shinde <shindesudarshan070@gmail.com>
2021-05-01 23:02:18 +0530

    local data base connect

--- app.js
+++ app.js
@@ -6,8 +6,18 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const mongoose = require('mongoose');
 
 var app = express();
+
+
+// local database connect
+mongoose.connect('mongodb://localhost:27017/cricket',
+  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true, },
+  (err) => {
+    console.log("connected", err ? err : true)
+  }
+)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,12 +33,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
