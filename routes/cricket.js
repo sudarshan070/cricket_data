fdbde86e8ed03c4c030bd5b4c6e1bdd587771840
Sudarshan shinde <shindesudarshan070@gmail.com>
2021-05-07 14:28:25 +0530

    odi data post route add

--- routes/cricket.js
+++ routes/cricket.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var router = express.Router();
 const Cricket = require('../modal/cricket')
+const OdiData = require('../modal/odidata')
+const testData = require('../modal/testData')
 
 router.post('/', async (req, res, next) => {
     try {
         let cricket = await Cricket.create(req.body)
         res.json({ cricket })
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.post('/odi', async (req, res, next) => {
+    try {
+        let odiData = await OdiData.create(req.body)
+        res.json({ odiData })
     } catch (error) {
         next(error)
     }
