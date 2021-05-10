e7b8f0eb06032a83d9da02f1e6e418420c8d54f0
Sudarshan shinde <shindesudarshan070@gmail.com>
2021-05-10 15:19:19 +0530

    extra info remove

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Route } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 import Matches from "./Matches";
@@ -9,36 +9,6 @@ import Records from "./Records";
 import Stats from "./Stats";
 
 export default function Dashboard({ info }) {
-  const [odiData, setOdiData] = useState([]);
-  const [testData, setTestData] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  const getOdiData = async () => {
-    try {
-      setLoading(true);
-      const odiData = await axios.get("/cricket/odi");
-      const data = odiData.data.odiData;
-      setOdiData(data);
-    } catch (error) {
-      setLoading("null");
-    }
-  };
-
-  const getTestData = async () => {
-    try {
-      setLoading(true);
-      const testData = await axios.get("/cricket/test");
-      const data = testData.data.testData;
-      setTestData(data);
-    } catch (error) {
-      setLoading("null");
-    }
-  };
-
-  useEffect(() => {
-    getOdiData();
-    getTestData();
-  }, []);
 
   return (
     <section className="pl-5 dashboard">
